e353ffbe21537d74e91f41ee1288f82379dc1864
orenhav <info.ohav.co.il>
2013-02-14 15:07:07 +0200

    init commit of files

---
+++ README.md
@@ -0,0 +1,2 @@
+# this site is a test of git repository
+# being tested by Itamar AND Nitai
---
+++ timi.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>hi</title>
+	</head>
+	<body>
+		<div id="main">
+			<h1>the title</h1>
+			<p>
+				this is the main site's content
+
+				<div> master added stuff here</div>		
+
+				itamar added his new code here
+			
+
+			</p>
+		</div>
+	</body>
+</html>
